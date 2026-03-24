f7194da71322bcb1e5fea5a25ad9e5e43f2efe8f
Akob Cripto <akobeduardovic318@gmail.com>
2026-03-24 18:38:46 +0300

    feat: add email/password registration with Firebase Auth

--- app/src/main/java/com/example/bookstoreapp/MainActivity.kt
+++ app/src/main/java/com/example/bookstoreapp/MainActivity.kt
@@ -4,17 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.google.firebase.firestore.FirebaseFirestore
-
+import com.example.bookstoreapp.ui.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val db = FirebaseFirestore.getInstance()
-            db.collection("Book").document().set(mapOf("name" to "My fav book"))
-
+            LoginScreen()
         }
     }
 }
